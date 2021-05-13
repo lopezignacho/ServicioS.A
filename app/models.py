21b5a379bff11f975168d2c0d1de3dcdedde78ea
lopezignacho <ignaciolpcode@gmail.com>
2021-05-13 06:45:19 -0400

    main v2

--- app/models.py
+++ app/models.py
@@ -17,14 +17,13 @@ opciones_consultas = [
     
 ]
 
-
 class Contacto(models.Model):
     nombre = models.CharField(max_length=50)
     correo = models.EmailField()
     tipo_consulta = models.IntegerField(choices=opciones_consultas)
     mensaje = models.TextField()
     avisos = models.BooleanField()
-    
+
 
     def __str__(self):
         return self.nombre 
